e03c2f0cf58956d89d48ca41a18448ca86f1d0bf
krokowski0x <krokowski.dx@gmail.com>
2018-03-29 14:05:21 +0200

    Added SASS support

--- webpack.config.js
+++ webpack.config.js
@@ -17,22 +17,14 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
+        test: /\.scss$/,
+        use: [ {
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        } ]
       }
     ]
   },
