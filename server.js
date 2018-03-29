179aac026dc44421bdbc846e3b4fb52047e817ff
krokowski0x <krokowski.dx@gmail.com>
2018-03-29 09:30:43 +0200

    Changed public folder to dist (for webpack)

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.listen(port, () => console.log('AVL Tree app is running on port 3000!'));
