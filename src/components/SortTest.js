28a62a423ad42bcd4d393b21836ee6932cb0e5a7
krokowski0x <krokowski.dx@gmail.com>
2018-03-31 00:15:51 +0200

    Refactored to class-based component

--- src/components/SortTest.js
+++ src/components/SortTest.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SortTest = (props) => {
-  return (
-    <div className='sort-tab'>
-      <h2>{props.match.params.sortType}</h2>
-    </div>
-  );
+class SortTest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: props.match.params.sortType,
+      arrays: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/arrays')
+      .then(res => res.json())
+      .then(arrays => this.setState({type: this.state.type, arrays: arrays.arrays}))
+      .then(console.log(this.state));
+  }
+
+  render() {
+    return (
+      <div className='sort-tab'>
+        <h2>{this.state.type}</h2>
+        <p></p>
+      </div>
+    );
+  };
 };
 
 export default SortTest;
