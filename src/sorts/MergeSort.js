dcb54f3535ecb9c7d3085eb009d5c05aadbb88c2
krokowski0x <krokowski.dx@gmail.com>
2018-04-01 11:29:55 +0200

    Added basic form of Merge Sort

---
+++ src/sorts/MergeSort.js
@@ -0,0 +1,29 @@
+Array.prototype.MergeSort = function() {
+    if (this.length < 2) {
+      return this;
+    }
+
+    const middle = Math.floor(this.length / 2);
+    const left = this.slice(0, middle);
+    const right = this.slice(middle);
+
+    return merge(left.MergeSort(),right.MergeSort());
+  }
+
+  function merge (left, right) {
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
+
+    while (indexLeft < left.length && indexRight < right.length) {
+      if (left[indexLeft] < right[indexRight]) {
+        result.push(left[indexLeft]);
+        indexLeft++;
+      } else {
+        result.push(right[indexRight]);
+        indexRight++;
+      }
+    }
+
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+  }
