33bc84a1f631a28fdad474efc44460d6a4e5b7f9
krokowski0x <krokowski.dx@gmail.com>
2018-04-05 10:48:10 +0200

    Added basic form of Intro Sort

---
+++ src/sorts/IntroSort.js
@@ -0,0 +1,21 @@
+import { HeapSort } from './HeapSort';
+import { InsertionSort } from './InsertionSort';
+import { QuickSort } from './QuickSort';
+
+Array.prototype.IntroSort = function() {
+  IntrospectiveSort(this, this.length, Math.floor(2*Math.log2(this.length)));
+  return this.InsertionSort();
+}
+
+function IntrospectiveSort (array, N, M) {
+  let i;
+  if (M <= 0) {
+    array.HeapSort();
+    return;
+  }
+  i = array.QuickSort();
+  if (i > 9)
+    IntrospectiveSort(array, i, M-1);
+  if (N-1-i > 9)
+    IntrospectiveSort(array+i+1, N-1-i, M-1);
+}
