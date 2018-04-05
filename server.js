963eafab157bae9d4282a90e87904a54522c17bf
krokowski0x <krokowski.dx@gmail.com>
2018-04-05 10:48:52 +0200

    Got rid of localStorage use (for now)

--- server.js
+++ server.js
@@ -4,35 +4,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
-app.get('/arrays', async (request, response) => {
-  try {
-    let xs = { len: 10000 };
-    let s = { len: 50000 };
-    let m = { len: 100000 };
-    let l = { len: 500000 };
-    let xl = { len: 1000000 };
-    let arrays = { xs, s, m, l, xl };
-    arrays = setup(arrays);
-    response.send({arrays});
-  } catch (err) {
-    response.status(400).send(err);
-  }
-});
+// app.get('/arrays', async (request, response) => {
+//   try {
+//     let xs = { len: 10000 };
+//     let s = { len: 50000 };
+//     let m = { len: 100000 };
+//     let l = { len: 500000 };
+//     let xl = { len: 1000000 };
+//     let arrays = { xs, s, m, l, xl };
+//     arrays = setup(arrays);
+//     response.send({arrays});
+//   } catch (err) {
+//     response.status(400).send(err);
+//   }
+// });
 
 app.listen(port, () => console.log('Sort Test app is running on port 3000!'));
-
-const setup = (arrays) => {
-  for (let array in arrays) {
-    arrays[array]['rand'] = Array.from({length: arrays[array]['len']}, () => Math.floor(Math.random() * arrays[array]['len']));
-
-    let a = arrays[array]['rand'];
-    arrays[array]['reverse'] = a.concat().sort((a,b) => b-a);
-    arrays[array]['25%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.25), ...a.concat().slice(0, arrays[array]['len']*0.75)];
-    arrays[array]['50%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.5), ...a.concat().slice(0, arrays[array]['len']*0.5)];
-    arrays[array]['75%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.75), ...a.concat().slice(0, arrays[array]['len']*0.25)];
-    arrays[array]['95%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.95), ...a.concat().slice(0, arrays[array]['len']*0.05)];
-    arrays[array]['99%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.99), ...a.concat().slice(0, arrays[array]['len']*0.01)];
-    arrays[array]['99,7%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.997), ...a.concat().slice(0, arrays[array]['len']*0.003)];
-  }
-  return arrays;
-}
--- src/components/SortTest.js
+++ src/components/SortTest.js
@@ -10,11 +10,27 @@ class SortTest extends Component {
   }
 
   componentDidMount() {
-    fetch('/arrays')
-      .then(res => res.json())
-      .then(arrays => this.setState({type: this.state.type, arrays: arrays.arrays}))
-      .then(console.log(this.state));
+    let xs = { len: 10000 };
+    let s = { len: 50000 };
+    let m = { len: 100000 };
+    let l = { len: 500000 };
+    let xl = { len: 1000000 };
+    let arrays = { xs, s, m, l, xl };
+    arrays = setup(arrays);
+    this.setState( {type: this.state.type, arrays} );
+    // const cachedArrays = localStorage.getItem('arrays');
+    // if (cachedArrays)
+    //   this.setState({ type: this.state.type, arrays: JSON.parse(cachedArrays) });
+    // else
+    //   fetch('/arrays')
+    //     .then(res => res.json())
+    //     .then(result => this.onSetResult(result, 'arrays'));
   }
+
+  // onSetResult(result, key) {
+  //   localStorage.setItem(key, JSON.stringify(result.arrays));
+  //   this.setState({ type: this.state.type, arrays: result.arrays });
+  // }
 
   render() {
     return (
@@ -27,3 +43,19 @@ class SortTest extends Component {
 };
 
 export default SortTest;
+
+const setup = (arrays) => {
+  for (let array in arrays) {
+    arrays[array]['rand'] = Array.from({length: arrays[array]['len']}, () => Math.floor(Math.random() * arrays[array]['len']));
+
+    let a = arrays[array]['rand'];
+    arrays[array]['reverse'] = a.concat().sort((a,b) => b-a);
+    arrays[array]['25%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.25), ...a.concat().slice(0, arrays[array]['len']*0.75)];
+    arrays[array]['50%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.5), ...a.concat().slice(0, arrays[array]['len']*0.5)];
+    arrays[array]['75%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.75), ...a.concat().slice(0, arrays[array]['len']*0.25)];
+    arrays[array]['95%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.95), ...a.concat().slice(0, arrays[array]['len']*0.05)];
+    arrays[array]['99%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.99), ...a.concat().slice(0, arrays[array]['len']*0.01)];
+    arrays[array]['99,7%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.997), ...a.concat().slice(0, arrays[array]['len']*0.003)];
+  }
+  return arrays;
+}
