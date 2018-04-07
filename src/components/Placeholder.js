b5c7bbbc7c40a81a002d0f2a123f20683a951f84
krokowski0x <krokowski.dx@gmail.com>
2018-04-07 10:13:23 +0200

    Added simple placeholder component

---
+++ src/components/Placeholder.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Facebook as Loader } from 'react-content-loader';
+
+const Placeholder = (props) => {
+    return <Loader />
+}
+
+export default Placeholder;
