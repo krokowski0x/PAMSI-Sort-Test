d5fd7c9c067a7fb190015a71fbe76607041062fa
krokowski0x <krokowski.dx@gmail.com>
2018-04-14 00:12:13 +0200

    Full implementation of the Wikipedia API (oh god, finally)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -25,40 +25,40 @@ class Dashboard extends Component {
   }
 
   render() {
-    if (this.state.arraysReady)
+    //if (this.state.arraysReady)
       return (
         <main>
           <DescriptionTab />
-          <Link to='bubble-sort'>
+          <Link to='Bubble_sort'>
             <SortTab type='Bubble Sort'/>
           </Link>
-          <Link to='insertion-sort'>
+          <Link to='Insertion_sort'>
             <SortTab type='Insertion Sort'/>
           </Link>
-          <Link to='heap-sort'>
+          <Link to='Heapsort'>
             <SortTab type='Heap Sort'/>
           </Link>
-          <Link to='merge-sort'>
+          <Link to='Merge_sort'>
             <SortTab type='Merge Sort'/>
           </Link>
-          <Link to='shell-sort'>
+          <Link to='Shellsort'>
             <SortTab type='Shell Sort'/>
           </Link>
-          <Link to='quick-sort'>
+          <Link to='Quicksort'>
             <SortTab type='Quick Sort'/>
           </Link>
-          <Link to='intro-sort'>
+          <Link to='Introsort'>
             <SortTab type='Intro Sort'/>
           </Link>
         </main>
       );
-    else
-      return (
-      <main>
-        <DescriptionTab />
-        <Placeholder />
-      </main>
-    );
+    // else
+    //   return (
+    //   <main>
+    //     <DescriptionTab />
+    //     <Placeholder />
+    //   </main>
+    // );
   }
 };
 
--- src/components/SortTest.js
+++ src/components/SortTest.js
@@ -8,25 +8,67 @@ const promiseWorker = new PromiseWorker(worker);
 class SortTest extends Component {
   constructor(props) {
     super(props);
-    this.onClick = this.handleClick.bind(this);
     this.worker = promiseWorker;
     this.state = {
       type: props.match.params.sortType
     };
   }
 
-  handleClick(e) {
-    this.worker.postMessage({work: 'Sort'})
-      .then(msg => console.log(msg));
+  componentDidMount() {
+    // Short summary and title fetch
+    fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&origin=*&titles=${this.state.type}`)
+    .then(res => res.json())
+    .then(data => {
+      let pageID = Object.keys(data.query.pages)[0];
+      let info = data.query.pages[pageID];
+      this.setState({title: info.title, description: info.extract, type: this.state.type});
+    })
+    .catch(err => console.log(err));
+
+    // Independent Infobox fetch
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=${this.state.type}&rvsection=0&origin=*`)
+    .then(res => res.json())
+    .then(data => {
+      let pageID = Object.keys(data.query.pages)[0];
+      let info = data.query.pages[pageID].revisions[0]['*'];
+
+      let worst = info.match(/time\s*=\s*.*/g) || 'Unknown';
+      let best = info.match(/best-time\s*=\s*.*/g) || 'Unknown';
+      let average = info.match(/average-time\s*=\s*.*/g) || 'Unknown';
+
+      // Had to break regex into few parts, becouse of inconsistent styling on Wikipedia
+      if (worst !== 'Unknown')
+        worst = 'O' + worst[0].match(/\(.*?\)/g)[0].replace(/\'\'s*/g,'');
+      if (best !== 'Unknown')
+        best = 'O' + best[0].match(/\(.*?\)/g)[0].replace(/\'\'s*/g,'');
+      if (average !== 'Unknown') {
+        // In Shellsort it was so badly formatted, that I had to have ths assertion
+        if (this.state.type === 'Shellsort')
+          average = 'Unknown';
+        else
+          average = 'O' + average[0].match(/\(.*?\)/g)[0].replace(/\'\'/g,'');
+      }
+       console.log(best,worst,average);
+       this.setState({average, best, worst})
+    })
+    .catch(err => console.log(err));
   }
 
   render() {
     return (
-      <div className='sort-dashboard'>
-        <h2>{this.state.type}</h2>
-        <button onClick={this.onClick}>Sort!</button>
+
+      <div className='sort-description'>
+        <h2>{this.state.title}</h2>
+        <p>{this.state.description}</p>
+        <div>
+          <h2>O(n)</h2>
+          <h4>Best case: {this.state.best}</h4>
+          <h4>Average case: {this.state.average}</h4>
+          <h4>Worst case: {this.state.worst}</h4>
+        </div>
+        <button>Sort!</button>
       </div>
-    )
+    );
   };
 };
 
