a0faa0f44a050d5c378aa5c5baf2ddaf70f4e5d4
krokowski0x <krokowski.dx@gmail.com>
2018-04-17 01:06:40 +0200

    Added particles in the background for fancy visual effect

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import Particles from 'react-particles-js';
+import particlesConfig from '../particles-config.json';
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import styles from './styles.scss';
 
 import Dashboard from './components/Dashboard';
 import SortTest from './components/SortTest';
 
+const history = createBrowserHistory();
+
 const App = () => {
   return (
-    <Router>
+    <Router history={history}>
       <div>
         <Route exact path='/' component={Dashboard} />
         <Route path='/:sortType' component={SortTest} />
+        <Particles config={particlesConfig} className='particles'/>
       </div>
     </Router>
   );
