ab730a017aed0fb9d2af8bae54c5d504349899cf
krokowski0x <krokowski.dx@gmail.com>
2018-04-16 14:27:55 +0200

    Added conditional sorting based on API route

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const { performance } = require('perf_hooks');
+const { CPUs } = require('os').cpus().length;
+const cluster = require('cluster');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,7 +19,7 @@ let m =  { len: 100000 };
 let l =  { len: 500000 };
 let xl = { len: 1000000 };
 let arrays = { xs, s, m, l, xl };
-let stats = { xs, s, m, l, xl };
+let stats = {'xs': {}, 's': {}, 'm': {}, 'l': {}, 'xl': {}};
 let arraysReady = false;
 
 app.use(express.static('dist'));
@@ -31,14 +33,23 @@ app.get('/arraysInit', (request, response) => {
   }
 });
 
-app.get('/stats', (request, response) => {
+app.get('/stats/:sortType', (request, response) => {
   try {
-    let statistics = runSorting();
-    response.status(200).send(statistics);
+    const type = request.params.sortType;
+    runSorting('rand', type);
+    runSorting('25%', type);
+    runSorting('50%', type);
+    runSorting('75%', type);
+    runSorting('95%', type);
+    runSorting('99%', type);
+    runSorting('99,7%', type);
+    runSorting('reverse', type);
+    response.status(200).send(stats);
   } catch (err) {
     response.status(400).send(err);
   }
 });
+
 
 app.listen(port, () => console.log('Sort Test app is running on port 3000!'));
 
@@ -47,12 +58,12 @@ const setup = (arrays) => {
     arrays[array]['rand'] = Array.from({length: arrays[array]['len']}, () => Math.floor(Math.random() * arrays[array]['len']));
 
     let a = arrays[array]['rand'];
-    arrays[array]['reverse'] = a.concat().sort((a,b) => b-a);
-    arrays[array]['25%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.25), ...a.concat().slice(0, arrays[array]['len']*0.75)];
-    arrays[array]['50%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.5), ...a.concat().slice(0, arrays[array]['len']*0.5)];
-    arrays[array]['75%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.75), ...a.concat().slice(0, arrays[array]['len']*0.25)];
-    arrays[array]['95%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.95), ...a.concat().slice(0, arrays[array]['len']*0.05)];
-    arrays[array]['99%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.99), ...a.concat().slice(0, arrays[array]['len']*0.01)];
+    arrays[array]['reverse'] =   a.concat().sort((a,b) => b-a);
+    arrays[array]['25%'] =   [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.25),  ...a.concat().slice(0, arrays[array]['len']*0.75)];
+    arrays[array]['50%'] =   [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.5),   ...a.concat().slice(0, arrays[array]['len']*0.5)];
+    arrays[array]['75%'] =   [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.75),  ...a.concat().slice(0, arrays[array]['len']*0.25)];
+    arrays[array]['95%'] =   [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.95),  ...a.concat().slice(0, arrays[array]['len']*0.05)];
+    arrays[array]['99%'] =   [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.99),  ...a.concat().slice(0, arrays[array]['len']*0.01)];
     arrays[array]['99,7%'] = [...a.concat().sort((a,b) => a-b).slice(0, arrays[array]['len']*0.997), ...a.concat().slice(0, arrays[array]['len']*0.003)];
 
     delete arrays[array]['len'];
@@ -62,18 +73,19 @@ const setup = (arrays) => {
   return arrays;
 }
 
-const runSorting = () => {
+const runSorting = (field, sortType) => {
   let start = 0;
   let end = 0;
   let time = 0;
 
   if (arraysReady) {
     for (let array in arrays) {
-        start = performance.now();
-        arrays[array]['rand'].QuickSort();
-        end = performance.now();
-        time = end - start;
-        stats[array] = `It took ${time.toFixed(3)} ms.`;
+      start = performance.now();
+      arrays[array][field][sortType]();
+      end = performance.now();
+      time = end - start;
+      console.log(time);
+      stats[array][field] = `It took ${time.toFixed(3)} ms.`;
     }
     return stats;
   } else {
