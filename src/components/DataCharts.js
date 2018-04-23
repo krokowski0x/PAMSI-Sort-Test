44135789275210bc76719a6893e363c28798a81f
krokowski0x <krokowski.dx@gmail.com>
2018-04-23 22:58:49 +0200

    Further styled chart lines and axis

--- src/components/DataCharts.js
+++ src/components/DataCharts.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Line } from 'react-chartjs-2';
 import Placeholder from './Placeholder';
 
@@ -9,7 +9,7 @@ function DataCharts(props) {
     'O(log n)':  labels.map(x => Math.log10(x)),
     'O(nlog n)': labels.map(x => x*Math.log10(x)),
     'O(n^2)':    labels.map(x => x**2)
-  }
+  };
 
   const opts = {
     maintainAspectRatio: false,
@@ -19,16 +19,30 @@ function DataCharts(props) {
                 // Max should be dynamically adjusted to the longest expected time
                 max: props.stats ? props.stats['reverse']['xl'] : 5000,
                 beginAtZero: true
+              },
+              scaleLabel: {
+                  display: true,
+                  fontColor: '#FFF',
+                  fontSize: 16,
+                  fontStyle: 'bold',
+                  labelString: 'Time (miliseconds)'
               }
           }],
           xAxes: [{
               ticks: {
                 // Skipping 10k arrays is better for chart clarity
                 min: 50
+              },
+              scaleLabel: {
+                  display: true,
+                  fontColor: '#FFF',
+                  fontSize: 16,
+                  fontStyle: 'bold',
+                  labelString: 'Array size (x1000)'
               }
           }]
       }
-  }
+  };
 
   const legend = {
     display: true,
@@ -39,7 +53,7 @@ function DataCharts(props) {
       fontSize: 16,
       fontStyle: 'bold'
     }
-  }
+  };
 
   if (props.stats)
     return (
@@ -50,42 +64,45 @@ function DataCharts(props) {
             label: 'Best Case',
             borderWidth: 6,
             borderColor: 'rgba(99,255,132,1)',
-            data: chartTypes[props.best]
+            data: chartTypes[props.best],
+            hidden: true
           },{
             label: 'Average Case',
             borderWidth: 6,
             borderColor: 'rgba(54, 162, 235, 1)',
-            data: chartTypes[props.average]
+            data: chartTypes[props.average],
+            hidden: true
           },{
             label: 'Worst Case',
             borderWidth: 6,
             borderColor: 'rgba(255,99,132,1)',
-            data: chartTypes[props.worst]
+            data: chartTypes[props.worst],
+            hidden: true
           },{
             label: 'Random order',
-            borderColor: '#E07BE0',
+            borderColor: '#DE369D',
             data: props.stats['rand']
           },{
             label: '25% sorted',
-            borderColor: '#381D2A',
+            borderColor: '#7DE2D1',
             data: props.stats['25%']
           },{
             label: '50% sorted',
-            borderColor: '#FFE900',
+            borderColor: '#EB6534',
             data: props.stats['50%']
           },
           {
             label: '75% sorted',
-            borderColor: '#E9E3B4 ',
+            borderColor: '#F3C98B ',
             data: props.stats['75%']
           },
           {
             label: '95% sorted',
-            borderColor: '#001D4A',
+            borderColor: '#C46D5E',
             data: props.stats['95%']
           },{
             label: '99% sorted',
-            borderColor: '#EF271B',
+            borderColor: '#BD1E1E',
             data: props.stats['99%']
           },{
             backgroundColor: 'rgba(0,0,0,0)',
@@ -95,7 +112,7 @@ function DataCharts(props) {
           },
           {
             label: 'Sorted in reverse order',
-            borderColor: '#EFF1ED',
+            borderColor: '#FFFFFF',
             data: props.stats['reverse']
           }
         ]}} options={opts} legend={legend} height={500}/>
